32f2629e32ca92d251295f5c847d1a06345ba266
WaterFace <kevinvisserweckworth@gmail.com>
2025-12-08 02:35:34 -0500

    day 8

--- src/day08.rs
+++ src/day08.rs
@@ -1,8 +1,145 @@
-pub fn part1(_input: &str) {
-    println!("Part 1 unimplemented.");
+use std::{
+    cmp::{self, Reverse},
+    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    fmt::Display,
+};
+
+use crate::util::{self, Vec3, ext::SortedExt};
+
+#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+struct JunctionBox {
+    pos: Vec3,
 }
 
-pub fn part2(_input: &str) {
-    println!("Part 2 unimplemented.");
+impl JunctionBox {
+    fn distance_to(self, other: Self) -> u64 {
+        (other.pos - self.pos).sqr_length_u64()
+    }
 }
 
+impl Display for JunctionBox {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{},{},{}", self.pos.x, self.pos.y, self.pos.z)
+    }
+}
+
+fn connected_components(distances: &[(u64, JunctionBox, JunctionBox)], n: usize) -> Vec<usize> {
+    let mut adjacency: HashMap<JunctionBox, Vec<JunctionBox>> = HashMap::new();
+    let mut vertices = HashSet::new();
+
+    vertices.extend(distances.iter().flat_map(|(_, a, b)| [a, b]));
+    for (_d, a, b) in distances.iter().take(n) {
+        adjacency.entry(*a).or_default().push(*b);
+        adjacency.entry(*b).or_default().push(*a);
+    }
+
+    // bfs over nodes in this subgraph to find connected components
+    let mut vertices: Vec<JunctionBox> = vertices.into_iter().collect();
+    let mut explored_components: HashSet<JunctionBox> = HashSet::new();
+    let mut explored: HashSet<JunctionBox> = HashSet::new();
+    let mut queue = VecDeque::new();
+
+    let mut component_sizes = BinaryHeap::new();
+
+    while let Some(v) = vertices.pop() {
+        if explored_components.contains(&v) {
+            continue;
+        } else {
+            explored_components.insert(v);
+        }
+        queue.clear();
+        queue.push_back(v);
+        explored.clear();
+        while let Some(v) = queue.pop_front() {
+            if explored.contains(&v) {
+                continue;
+            }
+            explored.insert(v);
+
+            for w in adjacency.entry(v).or_default() {
+                queue.push_back(*w);
+            }
+        }
+        explored_components.extend(&explored);
+        component_sizes.push(explored.len());
+    }
+
+    component_sizes.into_iter().collect()
+}
+
+pub fn part1(input: &str) {
+    let input = util::read_file(input).unwrap();
+
+    let junctions: Vec<_> = input
+        .lines()
+        .map(|line| {
+            let ints = util::parse_ints(line).unwrap();
+            assert!(ints.len() == 3);
+            JunctionBox {
+                pos: Vec3::new(ints[0], ints[1], ints[2]),
+            }
+        })
+        .collect();
+
+    let mut distances: Vec<(JunctionBox, JunctionBox, u64)> = Vec::new();
+    for i in 0..junctions.len() {
+        for j in i + 1..junctions.len() {
+            let a = junctions[i];
+            let b = junctions[j];
+            let (a, b) = (cmp::min(a, b), cmp::max(a, b));
+
+            distances.push((a, b, a.distance_to(b)));
+        }
+    }
+
+    let distances = distances
+        .into_iter()
+        .map(|(a, b, v)| (v, a, b))
+        .collect::<Vec<_>>()
+        .sorted();
+
+    let component_sizes = connected_components(&distances, 1000).sorted_by_key(|t| Reverse(*t));
+    println!("{}", component_sizes.into_iter().take(3).product::<usize>());
+}
+
+pub fn part2(input: &str) {
+    let input = util::read_file(input).unwrap();
+
+    let junctions: Vec<_> = input
+        .lines()
+        .map(|line| {
+            let ints = util::parse_ints(line).unwrap();
+            assert!(ints.len() == 3);
+            JunctionBox {
+                pos: Vec3::new(ints[0], ints[1], ints[2]),
+            }
+        })
+        .collect();
+
+    let mut distances: Vec<(JunctionBox, JunctionBox, u64)> = Vec::new();
+    for i in 0..junctions.len() {
+        for j in i + 1..junctions.len() {
+            let a = junctions[i];
+            let b = junctions[j];
+            let (a, b) = (cmp::min(a, b), cmp::max(a, b));
+
+            distances.push((a, b, a.distance_to(b)));
+        }
+    }
+
+    let distances = distances
+        .into_iter()
+        .map(|(a, b, v)| (v, a, b))
+        .collect::<Vec<_>>()
+        .sorted();
+
+    for i in 1000.. {
+        let component_sizes = connected_components(&distances, i + 1);
+        println!("{i}: {}", component_sizes.len());
+        if component_sizes.len() == 1 {
+            let (_, a, b) = distances[i];
+            println!("{a} <-> {b}: {}", a.pos.x as u64 * b.pos.x as u64);
+            break;
+        }
+    }
+}
